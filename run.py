4a72ff9a448e4bc4b8e06d1cf1a97a468d5e2cf8
Sipo Charles <sipocharles@yahoo.co.uk>
2020-03-29 16:10:15 +0100

    Updated IP

--- run.py
+++ run.py
@@ -112,7 +112,7 @@ def play(username):
 ## Runs the Application
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP', '127.0.0.1'),
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=os.environ.get('PORT', '5000'))
 
 # --------------
